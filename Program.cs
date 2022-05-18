6782c2ebb4293b8cca4d653b52ba799734968130
EmiliyaAndamova <EmiliyaAndamova@WPIPCM>
2022-05-18 15:24:19 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _220518_Homework
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+         {
+            int[] arr = { 1, 2, 3, 4 };
+
+            Console.WriteLine(arr.Contains(8) & arr.Contains(9));
+
+         } 
+    }
+}
